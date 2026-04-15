a18ff6b43d61f6f488a29d2eb810773613964b97
Vynsil <vincepalaming2108@gmail.com>
2026-04-15 19:48:56 +0800

    almost done

--- vince-portfolio/src/pages/Contact.jsx
+++ vince-portfolio/src/pages/Contact.jsx
@@ -1,8 +1,54 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faGithub,
+    faGitlab,
+    faFacebook,
+    faInstagram,
+    faLinkedin,
+    faEnvelope
+} from "@fortawesome/free-brands-svg-icons";
+import '../assets/css/contact.css';
+
 function Contact() {
     return (
         <main>
-            <h1>Contact Me</h1>
-            <p>This is the contact page of my portfolio.</p>
+            <div className="contact-page">
+                <h1>Let’s Connect</h1>
+
+                <div className="contact-grid">
+
+                    <a className="contact-card" href="https://github.com/" target="_blank">
+                        <FontAwesomeIcon icon={faGithub} />
+                        <p>GitHub</p>
+                    </a>
+
+                    <a className="contact-card" href="https://gitlab.com/" target="_blank">
+                        <FontAwesomeIcon icon={faGitlab} />
+                        <p>GitLab</p>
+                    </a>
+
+                    <a className="contact-card" href="https://facebook.com/" target="_blank">
+                        <FontAwesomeIcon icon={faFacebook} />
+                        <p>Facebook</p>
+                    </a>
+
+                    <a className="contact-card" href="mailto:yourgmail@gmail.com">
+                        <FontAwesomeIcon icon={faEnvelope} />
+                        <p>Gmail</p>
+                    </a>
+
+                    <a className="contact-card" href="https://instagram.com/" target="_blank">
+                        <FontAwesomeIcon icon={faInstagram} />
+                        <p>Instagram</p>
+                    </a>
+
+                    <a className="contact-card" href="https://linkedin.com/" target="_blank">
+                        <FontAwesomeIcon icon={faLinkedin} />
+                        <p>LinkedIn</p>
+                    </a>
+
+                </div>
+            </div>
         </main>
     );
 }
